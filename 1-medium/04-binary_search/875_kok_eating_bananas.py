a42baa7614064499784cec67ba3ba60af90e6ee6
Gianluca Rea <reagianluca97@gmail.com>
2025-03-06 13:14:43 +0100

    Binary Search Tree

--- 1-medium/04-binary_search/875_kok_eating_bananas.py
+++ 1-medium/04-binary_search/875_kok_eating_bananas.py
@@ -1,3 +1,7 @@
+#875 Koko Eating Bananas
+
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. 
+# The guards have gone and will come back in h hours.
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         def eatingBanana(mid) -> bool: 
