80a3f679b27191ca578e95a409af53a1d72be657
Gianluca Rea <reagianluca97@gmail.com>
2026-09-07 16:07:54 +0200

    20. Valid Parentheses

---
+++ 0020_valid_parenthesis.py
@@ -0,0 +1,23 @@
+# 20. Valid Parentheses
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch == "(" or ch == "[" or ch == "{":
+                stack.append(ch)
+            else:
+                if len(stack) == 0:
+                    return False
+                val = stack.pop()
+                if ((ch == "]" and val != "[") or (ch == ")" and val != "(") or (ch == "}" and val != "{")):
+                    return False
+
+
+        return len(stack) == 0
--- 0242_valid_anagram.py
+++
@@ -1,8 +0,0 @@
-# 242. Valid Anagram
-# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-from collections import Counter
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
